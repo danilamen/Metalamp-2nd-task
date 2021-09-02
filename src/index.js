fd4fc5dbe9a60f52a236e77e1ddcf7115594d4ed
khangvul <belo9063872680@gmail.com>
2021-09-02 21:39:13 +0300

    Added babel, css, postCss to webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./index.css";
+
 console.log({
   foo: "bar",
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,47 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+let mode = "development";
+let target = "web";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  target = "browserslist";
+}
 
 module.exports = {
-  mode: "development",
+  mode: mode,
+  target: target,
+
+  plugins: [new MiniCssExtractPlugin(), new CssMinimizerPlugin()],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          mode === "production" ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.js%/i,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+
+  devtool: "source-map",
 
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
     },
     port: 8081,
+    hot: true,
   },
 
   // output: {
